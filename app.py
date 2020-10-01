f9b109d85f1d005bf664c32a88fe825a49f077a3
Meryl Dominguez <meryldominguez@gmail.com>
2020-10-01 16:50:19 -0500

    imported toolbar, flask, correct configs for app.py, cleaned up template/home

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "123doggo"
+
+debug = DebugToolbarExtension(app)
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 
 boggle_game = Boggle()
