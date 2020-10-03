06776a58b06ff6c292b627056bd3e1fd072ee997
Meryl Dominguez <meryldominguez@gmail.com>
2020-10-03 12:24:31 -0500

    maybe working with a Boggle class in Js will help

--- app.py
+++ app.py
@@ -13,22 +13,23 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
 boggle_game = Boggle()
+good_words=[]
 
 
 @app.route("/", methods=["GET","POST"])
 def play_game():
     if request.method == 'GET':
-        """return the information for <user_id>"""
-        if session["board"]:
-            return render_template("home.html", board=session["board"])
-        else:
             board = boggle_game.make_board()
             session["board"]= board
             return render_template("home.html", board=board)
     if request.method == 'POST':
         word = request.form['word']
-        print(word)
-        return render_template("home.html", board = session["board"])
+        board= session["board"]
+        valid = boggle_game.check_valid_word(board,word)
+        if valid == "ok":
+            good_words.append(word)
+            return render_template("home.html", words=good_words, board=board)
+
     
 # @app.route("/wordcheck")
 # def word_check():
--- static/boggleApp.js
+++ static/boggleApp.js
@@ -1,6 +1,26 @@
-let Form = $("#guess-form")
-let FormBtn = $("#guess-form-btn")
+let guessForm = $("#guess-form")
+let guessFormBtn = $("#guess-form-btn")
 let guessWord = $("#guess")
 
+function handleWord(){
+    let word = guessWord.val()
+    return word
+}
 
 
+// https://stackoverflow.com/questions/44644290/how-to-properly-submit-form-to-flask-with-ajax
+
+guessForm.on("submit", async function(e){
+    e.preventDefault()
+    let word = handleWord()
+    $.post("/", {word:word})
+})
+
+class Boggle{
+    constructor(){
+
+    }
+    function submitWord(self) {
+        
+    }
+}
